fb77999f0dadb6468a3d897bd15ee2f0fcd5ab69
Edervaldo José de Souza Melo <edersouzamelo@gmail.com>
2026-08-17 09:16:06 -0400

    S1: separate core runtime from HTTP adapter

--- src/nemosine_mind/__init__.py
+++ src/nemosine_mind/__init__.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
 
+from importlib.metadata import PackageNotFoundError, version
+
 __all__ = ["__version__"]
 
-__version__ = "1.0.2"
+try:
+    __version__ = version("nemosine-mind")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
